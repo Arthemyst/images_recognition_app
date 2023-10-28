2f33fb5016e00e09e74bf1d5d2eb6daba5dccc62
Arthemyst <pawcio262@gmail.com>
2023-10-28 17:25:24 +0200

    modified scrape_google.py

--- src/scrape_google/scrape_google.py
+++ src/scrape_google/scrape_google.py
@@ -1,10 +1,18 @@
+from typing import List
+
 from simple_image_download import Downloader
 
 
-keywords = ["plane", "missile", "drone"]
-limit = 600
-response = Downloader()
-response.directory = './scrape_google/images/'
+def download_images(keywords: List[str], directory: str, images_limit: int):
+    response = Downloader()
+    response.directory = directory
 
-for keyword in keywords:
-    response.download(keyword, limit)
+    for keyword in keywords:
+        response.download(keyword, images_limit)
+
+
+if __name__ == '__main__':
+    keywords = ["plane", "missile", "drone"]
+    images_limit = 600
+    directory = './scrape_google/images/'
+    download_images(keywords, directory, images_limit)
